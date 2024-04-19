e279a019974c847219202cdf591c40afbf922864
Ford Stewart <ford.stewart333@gmail.com>
2024-04-19 09:55:20 -0400

    pretty much copying the Flipper-Plugin-Tutorial by DroomOne and tom21100227

---
+++ flippp.c
@@ -0,0 +1,122 @@
+#include <furi.h>
+#include <gui/gui.h>
+#include <input/input.h>
+#include <stdlib.h>
+
+typedef enum {
+    EventTypeTick,
+    EventTypeKey,
+} EventType;
+
+typedef struct {
+    EventType type;
+    InputEvent input;
+} PluginEvent;
+
+typedef struct {
+    int x;
+    int y;
+    FuriMutex* mutex;
+} PluginState;
+
+static void render_callback(Canvas* const canvas, void* ctx) {
+    PluginState* plugin_state = (PluginState*)ctx;
+    furi_mutex_acquire(plugin_state->mutex, FuriWaitForever);
+    if(plugin_state == NULL) {
+        return;
+    }
+    // border around the edge of the screen
+    canvas_draw_frame(canvas, 0, 0, 128, 64);
+
+    canvas_set_font(canvas, FontPrimary);
+    canvas_draw_str_aligned(
+        canvas, plugin_state->x, plugin_state->y, AlignRight, AlignBottom, "Flippp!");
+
+    furi_mutex_release(plugin_state->mutex);
+}
+
+static void input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
+    furi_assert(event_queue);
+
+    PluginEvent event = {.type = EventTypeKey, .input = *input_event};
+    furi_message_queue_put(event_queue, &event, FuriWaitForever);
+}
+
+static void flippp_state_init(PluginState* const plugin_state) {
+    plugin_state->x = 50;
+    plugin_state->y = 30;
+}
+
+int32_t flippp_app() {
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(PluginEvent));
+
+    PluginState* plugin_state = malloc(sizeof(PluginState));
+
+    flippp_state_init(plugin_state);
+
+    plugin_state->mutex = furi_mutex_alloc(FuriMutexTypeNormal);
+    if(!plugin_state->mutex) {
+        FURI_LOG_E("flippp", "cannot create mutex\r\n");
+        free(plugin_state);
+        return 255;
+    }
+
+    // Set system callbacks
+    ViewPort* view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, render_callback, plugin_state);
+    view_port_input_callback_set(view_port, input_callback, event_queue);
+
+    // Open GUI and register view_port
+    Gui* gui = furi_record_open("gui");
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+
+    PluginEvent event;
+    for(bool processing = true; processing;) {
+        FuriStatus event_status = furi_message_queue_get(event_queue, &event, 100);
+
+        furi_mutex_acquire(plugin_state->mutex, FuriWaitForever);
+
+        if(event_status == FuriStatusOk) {
+            // press events
+            if(event.type == EventTypeKey) {
+                if(event.input.type == InputTypePress) {
+                    switch(event.input.key) {
+                    case InputKeyUp:
+                        plugin_state->y--;
+                        break;
+                    case InputKeyDown:
+                        plugin_state->y++;
+                        break;
+                    case InputKeyRight:
+                        plugin_state->x++;
+                        break;
+                    case InputKeyLeft:
+                        plugin_state->x--;
+                        break;
+                    case InputKeyOk:
+                    case InputKeyBack:
+                        processing = false;
+                        break;
+                    default:
+                        break;
+                    }
+                }
+            }
+        } else {
+            FURI_LOG_D("flippp", "FuriMessageQueue: event timeout");
+            // event timeout
+        }
+
+        view_port_update(view_port);
+        furi_mutex_release(plugin_state->mutex);
+    }
+
+    view_port_enabled_set(view_port, false);
+    gui_remove_view_port(gui, view_port);
+    furi_record_close("gui");
+    view_port_free(view_port);
+    furi_message_queue_free(event_queue);
+    furi_mutex_free(plugin_state->mutex);
+
+    return 0;
+}
